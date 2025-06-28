8c92d95f26ff77030571c7ccca51b9168ff88490
Abhik Kumar <91840867+OneAbhik@users.noreply.github.com>
2025-06-28 17:18:57 +0000

    DataType Conversion

---
+++ 01_Basics/03_Conversion.js
@@ -0,0 +1,17 @@
+// let score = true//undefined//null//"33abc";
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let iSLoggedIn = 1;
+// iSLoggedIn = "";//"avk"
+// let booleanIsLoggedIn = Boolean(iSLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+let someNum = 33;
+let StringNumber = String(someNum);
+console.log(StringNumber);
