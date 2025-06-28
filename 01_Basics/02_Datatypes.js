c5d6bd89fb05ae90a35e19beabd3e07a27ca3b07
Abhik Kumar <91840867+OneAbhik@users.noreply.github.com>
2025-06-28 16:35:21 +0000

    Data_Types

---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,24 @@
+"Use Strict"; // Treat all JS code as newer version 
+// alert(3+3); // We are using Node Js not browser
+// code readability should be high 
+//MDN/TC39 documentation reference
+
+//DATA TYPES:-
+let name = "AVK";
+let age = 20;
+let iSLoggedIn = false
+let State;
+
+// NUMBER -> 2 to power 53
+// bigint 
+// String => ""
+// boolean => true/false
+// null => Standalone value
+// undefined => 
+// Symbol => unique
+// object 
+
+console.log(typeof"avk");
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
