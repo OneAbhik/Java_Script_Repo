b570f9a517bace6dab95947edb1630b35df5cdc7
Abhik Kumar <91840867+OneAbhik@users.noreply.github.com>
2025-06-28 16:04:20 +0000

    Variables

---
+++ 01_Basics/01_Variable.js
@@ -0,0 +1,17 @@
+const accountId = 144553;
+let accountEmail = "avk@google.com";
+var accountPassword = "12345"; //Never Use Var
+
+accountCity = "Jaipur";
+
+//accountId = 134454;
+accountPassword = "43434343";
+accountEmail = "abhik@google.com";
+accountCity = "Bengaluru";
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
+let accountState;
+console.log(accountState);
+
