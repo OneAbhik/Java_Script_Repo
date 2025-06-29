3782004434d73db9d0e165bd213b8dd78ef863c2
Abhik Kumar <91840867+OneAbhik@users.noreply.github.com>
2025-06-29 15:52:49 +0000

    Advance DataTypes

---
+++ 01_Basics/06_DataType_summary.js
@@ -0,0 +1,48 @@
+// Primitive (7 Types)
+
+// String , Number , Boolean , Null , Undefined , Symbol , BigInt
+
+// const score = 100;
+// console.log(typeof score);
+
+// const scoreValue = 100.3;
+// console.log(typeof scoreValue)
+
+// const isLoggedIn = false;
+// console.log(typeof isLoggedIn);
+
+// const outsideTemp = null;
+// console.log(typeof outsideTemp);
+
+// let UserEmail;
+// console.log(typeof UserEmail);
+
+// const id = Symbol('123');
+// console.log(typeof id);
+// const anotherId = Symbol('123');
+// console.log(typeof anotherId);
+
+// console.log(id === anotherId);
+
+// const bigNumber = 3425444254545234n;
+// console.log(typeof bigNumber);
+
+
+
+// Reference (Non Primitive)
+
+// Array , Objects , Functions
+
+// const heros = ["Spiderman","Ben10","Hulk"];
+// console.log(typeof heros);
+
+// let myObj = {
+//     name : "Abhik",
+//     age : 22
+// }
+// console.log(typeof myObj);
+
+const MyFunction = function(){
+    console.log("Hello World");
+}
+console.log(typeof MyFunction);
