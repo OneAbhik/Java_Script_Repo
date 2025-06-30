288f78c4f9c484230bb2e5fd313bb507b704cbad
Abhik Kumar <91840867+OneAbhik@users.noreply.github.com>
2025-06-30 17:26:27 +0000

    strings

---
+++ 01_Basics/08_Strings.js
@@ -0,0 +1,34 @@
+const name = "abhik"
+const repoCount = 50
+
+//console.log(name + repoCount + " value")
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
+
+const gameName = new String('abhikkr')
+
+//Prototype / Prototype Methods
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('t'));
+
+// const newString = gameName.substring(0,4);
+// console.log(newString);
+
+// const anotherString = gameName.slice(-6,4)
+// console.log(anotherString);
+
+// const newStringOne = "   abhi "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+const url = "https://abhik.com/abhik%20kumar"
+console.log(url.replace('%20','_'))
+console.log(url.includes('sundar'))
+console.log(gameName.split('-'))
+
